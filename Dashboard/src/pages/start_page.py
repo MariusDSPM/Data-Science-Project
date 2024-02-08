5dc9f13e75a11480a375a2ee071513edf3e8233a
Marius Knipp <marius.knipp@student.uni-tuebingen.de>
2024-02-08 20:57:59 +0100

    Links to pages on the start page

--- Dashboard/src/pages/start_page.py
+++ Dashboard/src/pages/start_page.py
@@ -12,4 +12,13 @@ layout = [html.H1("Do Large Language Models Behave like a Human?", className="pa
           html.P("""Large Language models hold huge potential for a wide range of applications either for private, but also for professional use. 
                     One possible question that is of especially interesting for market research, is whether these models behave human-like enough to be used as surrogates for 
                     human participants in experiments. This dashboard is a first attempt to answer this question."""),
-          html.P("Feel free to explore more pages using the navigation menu.")]
+          html.P("Feel free to explore more pages using the navigation menu."),
+          dcc.Markdown("""
+                       **The following pages are available:**
+                       * [Experiments](/overview) - Explore the results of our experiments 
+                       * [Chat Bot](/chat-bot) - Interact with LLMs to see how they behave (*API keys required*)
+                       * [Live Experiment](/live-experiment) - Conduct your own experiment and search for similarities between the behaviour of LLMs and humans (*API keys required*)
+                       """
+                
+            ),
+          ]
